7fa480ae06429b760e6e272be103d8ac789e3c1a
suneelkumarprathipati <98082588+suneelkumarprathipati@users.noreply.github.com>
2023-06-26 16:34:32 +0530

    Update 620. Not Boring Movies.sql

--- 620. Not Boring Movies.sql
+++ 620. Not Boring Movies.sql
@@ -1,5 +1,2 @@
-select distinct p.product_id, round(sum(price * units) / sum(units), 2) as average_price from
-Prices as p join UnitsSold as U on
-p.product_id  = u.product_id and u.purchase_date between p.start_date and p.end_date
-group by p.product_id
-order by p.product_id;
+select * from cinema where mod(id,2) != 0 and description != "boring"
+order by rating desc;
