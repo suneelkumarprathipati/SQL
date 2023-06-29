475db94d013ee25c54274c4e50daccfdb3fe6410
SUNEEL KUMAR <98082588+suneelkumarprathipati@users.noreply.github.com>
2023-06-30 01:27:51 +0530

    Create 1789. Primary Department for Each Employee.sql

---
+++ 1789. Primary Department for Each Employee.sql
@@ -0,0 +1,3 @@
+select  employee_id , department_id from employee where primary_flag = 'Y'    
+union
+select  employee_id , department_id from employee where employee_id  not in (select  distinct employee_id from employee where primary_flag = 'Y'  )
